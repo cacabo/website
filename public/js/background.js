64fc3e9ca348924f7cada2c73bbbd54260522248
ccabo1 <ccabo@seas.upenn.edu>
2019-01-07 13:36:13 -0800

    updates and cleanup

--- public/js/background.js
+++ public/js/background.js
@@ -1,15 +1,15 @@
-$('document').ready(function() {
-  $(window).scroll(function() {
-    var doc = document.documentElement;
-    var pos = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-
-    var top = (pos / 12) + 40;
-    $('#line-1').css('top', top + 'px');
-
-    var left = (pos / 100) + 200;
-    $('#line-2').css('left', left + 'px');
-
-    var right = (pos / 110) + 250;
-    $('#line-3').css('right', right + 'px');
-  });
-});
+// $('document').ready(function() {
+//   $(window).scroll(function() {
+//     var doc = document.documentElement;
+//     var pos = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+//
+//     var top = (pos / 12) + 40;
+//     $('#line-1').css('top', top + 'px');
+//
+//     var left = (pos / 100) + 200;
+//     $('#line-2').css('left', left + 'px');
+//
+//     var right = (pos / 110) + 250;
+//     $('#line-3').css('right', right + 'px');
+//   });
+// });
